b0f615371118ed6a58b66efe3f8fd3973d276246
andreasLindqvist <andreas.lindqvist@teknikhuset.se>
2016-09-16 13:58:49 +0200

    fggf

--- web/src/common.js
+++ web/src/common.js
@@ -3,19 +3,21 @@ var apa = 123;
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import TestComponent from './components/TestComponent';
 import Tournament from './components/Tournament';
+import GameForm from './components/GameForm';
 
-console.log('apan3');
+console.log('Common');
 
 ReactDOM.render((
 <Router>
     <Route path="/apa" component={TestComponent} />
     <Route path="/tournament/:id" component={Tournament} />
 </Router>
-),document.querySelector('#app'))
+),document.querySelector('#app'));
     //getElementById('app'))
-
+ReactDOM.render(<GameForm />, document.querySelector('#sectionGameForm'));
 
 
 
