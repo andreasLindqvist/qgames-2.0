d4c81f286b889ad734f41336dff214f13bccd6c1
andreasLindqvist <andreas.lindqvist@teknikhuset.se>
2016-09-20 15:06:41 +0200

    nfghgh

---
+++ web/src/components/GamesPlayed.jsx
@@ -0,0 +1,28 @@
+﻿import config from '../config.json';
+import React from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+
+export default class GamesPlayed extends React.Component {
+    constructor() {
+        super();
+        this.state = { data: {} };
+    }
+    componentDidMount() {
+        this.getGamesPlayed();
+    }
+    getGamesPlayed() {
+        console.log('getGamesPlayed');
+        let url = `${config.webapi.tournaments}/${this.props.params.id}/games/played`;
+        fetch(url)
+        .then(function(response) {
+            return response.json()
+        })
+        .then(function(json) {
+            console.log('json', json);
+            this.setState({ data: data });
+        });
+    }
+    render() {
+                        
+    }
+}
