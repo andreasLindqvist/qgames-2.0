eba3bdb89b23f3a1a0f99433440bd179dd0e2431
andreasLindqvist <andreas.lindqvist@teknikhuset.se>
2016-09-21 09:54:14 +0200

    GamesPlayed

--- web/src/components/GamesPlayed.jsx
+++ web/src/components/GamesPlayed.jsx
@@ -7,22 +7,54 @@ export default class GamesPlayed extends React.Component {
         super();
         this.state = { data: {} };
     }
-    componentDidMount() {
-        this.getGamesPlayed();
-    }
-    getGamesPlayed() {
-        console.log('getGamesPlayed');
-        let url = `${config.webapi.tournaments}/${this.props.params.id}/games/played`;
-        fetch(url)
-        .then(function(response) {
-            return response.json()
-        })
-        .then(function(json) {
-            console.log('json', json);
-            this.setState({ data: data });
-        });
-    }
+    //componentDidMount() {
+    //    this.getGamesPlayed();
+    //}
+    //getGamesPlayed() {
+    //    console.log('getGamesPlayed', this.props.id);
+    //    let url = `${config.webapi.tournaments}/${this.props.id}/games/played`;
+    //    fetch(url)
+    //    .then(function(response) {
+    //        return response.json()
+    //    })
+    //    .then((json) => {
+    //        console.log('json', json);
+    //        this.setState({ data: json });
+    //    });
+    //}
     render() {
-                        
+        console.log('GamesPlayed render', this.props.id);
+        return (
+                        <ul>
+                {this.state.data.map(function(game) {
+                return (
+                    <li key={game._id} className="table">
+                        <div className="team">
+                            <span className="column logo">
+                                //<img src="{game.}" game />
+                            </span>
+                            <span className="column name"></span>
+                            <span className="column goals"></span>
+                        </div>
+                        <div className="team">
+                            <span className="column logo">
+                                //<img src="{game.}" game />
+                            </span>
+                            <span className="column name"></span>
+                            <span className="column goals"></span>
+                        </div>
+                    </li>
+                )
+                }
+                )}
+            </ul>
+//                                <ul>
+//                        {this.state.data.map(function(tournament) {
+//                            return (
+//                                <li key={tournament._id} data-id={tournament.id}><a href={`/#/tournament/${tournament._id}`}>{tournament.name}</a></li>
+//                                );
+//})}
+//                    </ul>             
+        )
     }
 }
